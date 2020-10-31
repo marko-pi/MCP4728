1e1f36c16fa5c9647c0951fdd3b2fce37bdc8898
marko-pi <marko@pinteric.com>
2020-10-31 01:15:36 +0100

    Finish initial commit

---
+++ MCP4728_test.py
@@ -0,0 +1,114 @@
+import time
+from ctypes import cdll, c_int, c_float, c_bool, c_void_p, POINTER
+
+class chippointer(c_void_p):
+    pass
+
+# Wrapping C library
+MCP4728 = cdll.LoadLibrary("./MCP4728.so")
+
+"""
+# object = initialise(sda, scl, ldac, address)
+
+Creates an instance of the chip.  The initialisation type depends on the SDA, SCL and LDAC pins provided.  If LDAC is irregular, reading and writing the chip address is disabled.  If SDA and SCL do not match the predefined I2C pins, changing the output voltages is disabled.  Both i2c-0 and i2c-1 busses can be used.  Do not forget to provide pull-up resistors for the LDAC line and the I2C-0 lines.
+
+Examples:
+
+chip = initialise(2,3,-1,0x61)       # opens an instance of the chip at address 0x61 and allows changing the output voltages over the i2c-1 bus (the chip address is mandatory)
+chip = initialise(16,20,21,-1)       # opens an instance of the chip at an unknown address and allows reading/writing the address
+chip = initialise(0,1,16,-1)         # opens an instance of the chip at an unknown address and allows both reading/writing the address and changing the output voltages over the i2c-0 bus
+"""
+initialise = MCP4728.initialise
+initialise.restype = chippointer
+initialise.argtypes = [c_int, c_int, c_int, c_int]
+
+"""
+deinitialise(object)
+
+Removes the instance of the chip.  Recommended but not mandatory at the end of the program.
+"""
+deinitialise = MCP4728.deinitialise
+deinitialise.argtypes = [chippointer]
+
+"""
+address = getaddress(object)
+
+Reads the current chip address.  The obtained address is automatically stored by the library.  LDAC mandatory.
+"""
+getaddress = MCP4728.getaddress
+
+"""
+setaddress(object, address)
+
+Writes the new chip address.  The current chip address must be supplied by the programmer or read with getaddress beforehand.  LDAC mandatory.
+"""
+setaddress = MCP4728.setaddress
+setaddress.argtypes = [chippointer, c_int]
+
+"""
+singleinternal(object, channel, voltage, eeprom)
+
+Sets the absolute voltange on the channel using the internal voltage reference.  The eeprom indicates whether the voltage is written to the EEPROM of the chip.  SDA/SLC must be 0/1 or 2/3.
+"""
+singleinternal = MCP4728.singleinternal
+singleinternal.argtypes = [chippointer, c_int, c_float, c_bool]
+
+"""
+singleexternal(object, channel, voltage, eeprom)
+
+Sets the relative voltange on the channel using the external voltage reference.  The eeprom indicates whether the voltage is written to the EEPROM of the chip.  SDA/SLC must be 0/1 or 2/3.
+"""
+singleexternal = MCP4728.singleexternal
+singleexternal.argtypes = [chippointer, c_int, c_float, c_bool]
+
+"""
+multipleinternal(object, [voltages], eeprom)
+
+Sets the absolute voltanges on all four channels using the internal voltage reference.  The eeprom indicates whether the voltages are written to the EEPROM of the chip.  SDA/SLC must be 0/1 or 2/3.
+"""
+multipleinternal = MCP4728.multipleinternal
+multipleinternal.argtypes = [chippointer, POINTER(c_float), c_bool]
+
+"""
+multipleexternal(object, [voltages], eeprom)
+
+Sets the relative voltanges on all four channels using the external voltage reference.  The eeprom indicates whether the voltages are written to the EEPROM of the chip.  SDA/SLC must be 0/1 or 2/3.
+"""
+multipleexternal = MCP4728.multipleexternal
+multipleexternal.argtypes = [chippointer, POINTER(c_float), c_bool]
+
+
+###### TEST EXAMPLE ######
+
+# creates instances of two chips with unknown addresses, both reading/writing the address and changing the output voltages over the i2c-1 bus allowed.
+chip1 = initialise(2,3,16,0)
+chip2 = initialise(2,3,20,0)
+
+# gets both addresses
+res1 = getaddress(chip1)
+res2 = getaddress(chip2)
+
+print('Chip I2C addresses: 0x%x 0x%x' % (res1, res2))
+
+# sets four absolute voltages on the first chip
+voltages=[1.0,1.5,2.0,2.5]
+floats = (c_float*4)(*voltages)
+multipleinternal(chip1,floats,False)
+
+# sets four absolute voltages on the second chip
+voltages=[0.5,1.0,1.5,2.0]
+floats = (c_float*4)(*voltages)
+multipleinternal(chip2,floats,False)
+
+# sets one absolute voltage on the first chip on channel 4
+singleinternal(chip1,4,3.0,False)
+
+# sets one absolute voltage on the second chip on channel 4
+singleinternal(chip2,4,2.5,False)
+
+# removes instances of two chips
+deinitialise(chip1)
+deinitialise(chip2)
+
+# this should be added if voltages are written to the EEPROM accoring to the manual
+time.sleep(0.05)
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MCP4728
-Raspberry Pi library for the MCP4728 chip.  It allows both reading/writing the address using three arbitrary GPIO pins and changing the output voltages using the i2c-0 and i2c-1 buses.
+Raspberry Pi library for the MCP4728 Quad DAC chip.  It allows
+- reading/writing the address by bitbanging at three arbitrary GPIO pins,
+- changing the output voltages by kernel procedures on both i2c-0 and i2c-1 busses.
+
+The core of the project is an OOP (object oriented programming) compatible C library (MCP4728.so).  There are two reasons for this step:
+- There is no need for any other library (RPi.GPIO, pigpio...) to execute the code;
+- The bitbanging part of the library is much faster and time consistent (see below).
+
+Reading and writing address for the MCP4728 chip requires an additional LDAC line and is not I2C protocol compliant.  Therefore bitbanging must be used.  The target frequency for bitbanging is 100 kHz, but the de facto frequency is about 90 kHz.  Executing C programs is simply not as efficient as using kernel procedures.
+
+![PulseView screenshot](/two_chips_read_address.png)
+
+The output voltages are changed uses kernel procedures.
+
+On [this page](http://www.pinteric.com/raspberry.html#dac) you will find the application circuits with some additional instructions.
+
+The library provides eight public functions: two functions for initialisation/de-initialisation, two functions for reading/writing the address and four functions for changing the output voltages.  The description of the functions is given in the Python test file (MCP4728_test.py).
+
+A simple Python script is provided as an example of how to use the library. A proper Python library could easily be created to wrap up all procedures into a standard Python class. Since I assume that only advanced users will use this library, I think that the Python example script will be sufficient.
+
+When the chip operates in internal source mode, it offers gain=1 for voltages up to 2.048 and gain=2 for voltages up to 4.096 or VDD, whichever is smaller.  The library makes automatic choice there, gain=1 when a voltage up to 2 V is requested and gain=2 when a voltage above 2 V is requested.
